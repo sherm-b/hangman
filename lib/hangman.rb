a113ff7eef967ef5c4188b7c7ac730c509f41c9f
Jeremy <jeremy.steven.bess@gmail.com>
2022-10-08 13:07:53 -0700

    Create methods to get a keyword from dictionary file, accept user input, and test it against the keyword

---
+++ lib/hangman.rb
@@ -0,0 +1,49 @@
+
+class Hangman
+    def initialize
+        puts 'HANGMAN\n\n'
+        @keyword = get_random_word
+        @template = Array.new(@keyword.length, ' _ ')
+        @used_letters_incorrect = Array.new
+        @man = 0
+    end
+
+    def get_random_word
+        dictionary = File.open('google-10000-english-no-swears.txt', 'r')
+        File.readlines(dictionary).sample.upcase
+    end
+
+    def valid_input?(char)
+        unless @used_letters_incorrect.contain?(char) && @template.contain?(" #{char} ") && char.length != 1
+            return true
+        end
+    end
+
+    def guess_check(guess)
+        @keyword.each_char.with_index do |char, index|
+            if guess == char
+                @template[index] = " #{char} "
+            else
+                @man += 1
+                @used_letters_incorrect << user_input
+            end
+        end
+    end
+
+    def user_turn
+        puts "\n\nYou've made #{man}/5 mistakes! Be careful!\n\n"
+        puts @template
+        puts "Mistakes: #{@used_letters_incorrect}\n\n"
+        puts "Enter your letter!"
+        user_input = gets.chomp.upcase
+        if !valid_input?(user_input)
+            puts "Invalid input! You can't use a letter you've already used or more than one letter!"
+            return
+        else
+            guess_check(user_input)
+        end
+    end
+        
+end
+
+hangman = Hangman.new
